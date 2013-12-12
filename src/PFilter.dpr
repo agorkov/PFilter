c6d8964cc384598d0ed01f3d0af4092cab067810
Alexey <gorkoff@gmail.com>
2013-12-12 21:39:39 +0400

    Initial commit

---
+++ src/PFilter.dpr
@@ -0,0 +1,14 @@
+program PFilter;
+
+uses
+  Vcl.Forms,
+  UFMain in 'UFMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
